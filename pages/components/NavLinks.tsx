10ecfdef8f6199c6c0652c5d258d893609388490
preciousimo <preciousimoniakemu@gmail.com>
2022-12-17 22:11:58 +0100

    Build Nav Links components

--- pages/components/NavLinks.tsx
+++ pages/components/NavLinks.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+// 'use client'
+
+import { categories } from "../../constants"
+import { usePathname } from "next/navigation";
+import NavLink from "./NavLink"
 
 function NavLinks() {
-  return (
-    <nav>NavLinks</nav>
-  )
+    const pathname = usePathname();
+
+    const isActive = (path: string) => {
+        return pathname?.split('/').pop() === path;
+    }
+    return (
+        <nav className="grid grid-cols-4 md:grid-cols-7 text-xs md:text-sm gap-4 pb-10 max-w-6xl mx-auto border-b">
+            {categories.map((category) => (
+                <NavLink
+                    key={category}
+                    category={category}
+                    isActive={isActive(category)} 
+                />
+            ))}
+        </nav>
+    );
 }
 
 export default NavLinks
