6325a192121ea5a56c29c5e51256eb8580c7b609
preciousimo <preciousimoniakemu@gmail.com>
2022-12-17 23:45:09 +0100

    Build fetch news function

---
+++ utils/fetchNews.ts
@@ -0,0 +1,13 @@
+const fetchNews = async (
+    category?: Category | string,
+    keywords?: string,
+    isDynamic?: boolean
+) => {
+    // GraphQL query
+
+    // Fetch function with Next.js 13 catching...
+
+    // Sort function by images vs not images present
+
+    // return result
+}
