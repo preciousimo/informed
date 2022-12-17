62383ba1f520b16914042e831031b418fdd583ea
preciousimo <preciousimoniakemu@gmail.com>
2022-12-18 00:06:23 +0100

    Implement mediastack API

--- pages/components/HomePage.tsx
+++ pages/components/HomePage.tsx
@@ -1,8 +1,8 @@
 import { categories } from "../../constants"
 import fetchNews from "../../utils/fetchNews"
 
-async function HomePage() {
-  const news: NewsResponse = await fetchNews(categories.join(','))
+function HomePage() {
+  // const news: NewsResponse = await fetchNews(categories.join(','))
   return (
     <div>HomePage</div>
   )
